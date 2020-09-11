94ca4ba075403d9ce8eabbb921d836a0785d9108
sree1210 <sreeharsha.1210@gmail.com>
2020-09-11 13:49:28 +0530

    Student DTO Added (Ver-2.0)

---
+++ GitDemo/src/main/java/com/dto/Student.java
@@ -0,0 +1,19 @@
+package com.dto;
+
+public class Student {
+	private int sno;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getSno() {
+		return sno;
+	}
+
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	
+}
